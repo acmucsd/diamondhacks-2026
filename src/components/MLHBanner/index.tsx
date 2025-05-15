030275bef8a6e596ed3f9375f8688a56c00c54f9
Angela Hu <angelahu925@gmail.com>
2025-05-14 17:17:33 -0700

    Added mlh banner

--- src/components/MLHBanner/index.tsx
+++ src/components/MLHBanner/index.tsx
@@ -5,12 +5,12 @@ import styles from './style.module.scss';
 const MLHBanner: React.FC = () => {
   return (
     <a
-      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=blue"
+      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=blue"
       target="_blank"
     >
       <Image
-        src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-blue.svg"
-        alt="Major League Hacking 2025 Hackathon Season"
+        src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-blue.svg"
+        alt="Major League Hacking 2026 Hackathon Season"
         width={150}
         height={150}
         className={styles.banner}
