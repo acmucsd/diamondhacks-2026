ae2c93fb39b1ecd27f17271ded94bdace4ea51b0
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2025-11-17 12:30:01 -0800

    Add decorations back to Room3

--- src/sections/Room3/index.tsx
+++ src/sections/Room3/index.tsx
@@ -1,22 +1,22 @@
 import { QUESTIONS } from './questions';
 import styles from './style.module.scss';
 import FAQAccordion from '@/components/FAQAccordion';
-import Shelf from '/public/assets/Shelf.svg';
-import Cauldron from '/public/assets/cauldron-steaming.svg';
+import Room3Decor from '/public/assets/Room3Decor.svg';
+import Room3Crack from '/public/assets/Room3Crack.svg';
 import Typography from '@/components/Typography';
 const Room3: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.trapezoidOuter} id="faq">
+        <Room3Decor className={styles.decor} />
         <div className={styles.trapezoidInner}> 
-          <Shelf className={styles.shelf} />
-          <Cauldron className={styles.cauldron} />
-          <div className={styles.faqContent}>
-            <Typography variant="display/light/medium" className={styles.title}>
-              Frequently asked questions
-            </Typography>
-            <FAQAccordion data={QUESTIONS} className={styles.faqContainer} />
-          </div>
+          <Room3Crack className={styles.crack} />
+        </div>
+        <div className={styles.faqContent}>
+          <Typography variant="display/light/medium" className={styles.title}>
+            Frequently asked questions
+          </Typography>
+          <FAQAccordion data={QUESTIONS} className={styles.faqContainer} />
         </div>
       </div>
     </div>
